3d38397f41a953bfb00ef22862aaf74978c89b0a
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-11-17 20:35:02 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
 
-minimum_val = 0
+minimum_val = 1
 maximum_val = 100
 
+number_of_trys = 0
 
 # Функция is_valid() проверяет введенное пользователем число
 # проверяет, что 1 <= число <= 100, а также, что число не содержит
@@ -14,14 +15,13 @@ def is_valid(text):
       return False
       
   # Проверка на диапазон
-  if not minimum_val < int(text) <= maximum_val:
+  if not minimum_val <= int(text) <= maximum_val:
     return False
 
   return True
-
-
+  
 # Генеририруем случайное число
-num = random.randint(1, 100)
+num = random.randint(minimum_val, maximum_val)
 
 print("Добро пожаловать в числовую угадайку!")
 
@@ -32,5 +32,20 @@ while True:
     print("А может быть все-таки введем целое число от 1 до 100?")
     continue
   new_number = int(user_string)
-  print(new_number)
-  break
+  number_of_trys += 1
+  if new_number < num:
+    print("Ваше число меньше загаданного, попробуйте еще разок")
+  elif new_number > num:
+    print("Ваше число больше загаданного, попробуйте еще разок")
+  else:
+    print("Вы угадали, поздравляем!")
+    print("Количество попыток:", number_of_trys)
+    
+    if input("Хотите сыграть снова? y - да, n - нет \n") == "y":
+      # Генеририруем новое случайное число
+      num = random.randint(minimum_val, maximum_val)
+      # Обнуляем счётчик попыток
+      number_of_trys = 0
+    else:
+      print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
+      break
